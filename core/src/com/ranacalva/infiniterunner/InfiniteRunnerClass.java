485c1d194d4aa544e1092f61a331afc371b08f62
Uriel Carrillo <carrillouriel@gmail.com>
2016-01-26 20:50:13 -0700

    Package name was wrong, fixed it

---
+++ core/src/com/ranacalva/infiniterunner/InfiniteRunnerClass.java
@@ -0,0 +1,21 @@
+package com.ranacalva.infiniterunner;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class InfiniteRunnerClass extends ApplicationAdapter {
+
+	
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void render () {
+
+	}
+}
