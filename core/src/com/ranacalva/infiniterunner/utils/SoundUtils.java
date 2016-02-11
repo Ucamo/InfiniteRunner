794c1b0459eeab7b4426bdb57d9dd2c7bab99ef6
Uriel Carrillo <carrillouriel@gmail.com>
2016-02-10 23:54:55 -0700

    Now it runs on android

--- core/src/com/ranacalva/infiniterunner/utils/SoundUtils.java
+++ core/src/com/ranacalva/infiniterunner/utils/SoundUtils.java
@@ -1,13 +1,15 @@
 package com.ranacalva.infiniterunner.utils;
 
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 
 public class SoundUtils {
 
-    Sound jump = Gdx.audio.newSound(Gdx.files.internal("sounds/jump.wav"));
-    Sound hit = Gdx.audio.newSound(Gdx.files.internal("sounds/hit_hurt.wav"));
+    Sound jump = Gdx.audio.newSound(Gdx.files.getFileHandle("data/Jump.wav", Files.FileType.Internal));
+    Sound hit = Gdx.audio.newSound(Gdx.files.getFileHandle("data/Hit_Hurt.wav", Files.FileType.Internal));
+
 
     public void PlaySound_Jump(){
         jump.play();
